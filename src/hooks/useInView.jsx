db20cc14b5efc0dda9288535d1de1ce7adeb72a7
saeed ahmad <119321424+saeed66699@users.noreply.github.com>
2026-05-25 18:42:56 +0500

    Create useInView.jsx

---
+++ src/hooks/useInView.jsx
@@ -0,0 +1,31 @@
+// import { useState, useEffect, useRef } from 'react';
+
+// const useInView = (threshold = 0.1) => {
+//   const [isInView, setIsInView] = useState(false);
+//   const ref = useRef(null);
+
+//   useEffect(() => {
+//     const observer = new IntersectionObserver(
+//       ([entry]) => {
+//         if (entry.isIntersecting) {
+//           setIsInView(true);
+//         }
+//       },
+//       { threshold }
+//     );
+
+//     if (ref.current) {
+//       observer.observe(ref.current);
+//     }
+
+//     return () => {
+//       if (ref.current) {
+//         observer.unobserve(ref.current);
+//       }
+//     };
+//   }, [threshold]);
+
+//   return [ref, isInView];
+// };
+
+// export default useInView;
